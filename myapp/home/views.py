bcbd9d99d1871c5cc47f1ae4a096a28394cadf97
Manav Goel <manav.goel90@gmail.com>
2012-08-16 18:27:29 +0530

    Created home blueprint and home.html

--- myapp/home/views.py
+++ myapp/home/views.py
@@ -1,4 +1,13 @@
 """Create home blueprint  and all the views in it here.
 """
 
-from flask import Blueprint
+from flask import Blueprint, render_template
+
+mod = Blueprint('home', __name__,
+                template_folder='templates',
+                static_foler='static')
+
+
+@mod.route('/')
+def home():
+    return render_template('home/home.html')
